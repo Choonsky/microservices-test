e9d63bfdcb4cb06360a29647f737e71817828ac6
Stanislav Nemirovsky <choonsky@gmail.com>
2022-11-09 11:40:48 +0600

    Eureka clients added

--- product-service/src/main/java/com/nemirovsky/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/nemirovsky/productservice/controller/ProductController.java
@@ -1,17 +1,22 @@
 package com.nemirovsky.productservice.controller;
 
 import com.nemirovsky.productservice.service.ProductService;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import lombok.RequiredArgsConstructor;
 import org.json.JSONObject;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/product")
 public class ProductController {
+
+    private final DiscoveryClient discoveryClient;
 
     private final ProductService productService;
 
@@ -19,6 +24,11 @@ public class ProductController {
     @ResponseStatus(HttpStatus.OK)
     public JSONObject findProductInfo(@PathVariable String productId) throws IOException, InterruptedException {
         return productService.findByProductId(productId);
+    }
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
     }
 
 }
